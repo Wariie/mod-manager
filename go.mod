949ba74a4e0c2536efef87a1d735a9906383209e
Wariie <guilhem.mateo12@gmail.com>
2020-07-23 18:27:57 +0200

    First Commit

---
+++ go.mod
@@ -0,0 +1,12 @@
+module github.com/Wariie/mod-manager
+
+go 1.14
+
+replace guilhem-mateo.fr/modbase/modules => ../modbase/modules
+
+require (
+	github.com/Wariie/go-woxy/com v0.0.0-20200723141448-9ec36fce8314
+	github.com/Wariie/go-woxy/modbase v0.0.0-20200723144132-450c7adb5ed0
+	github.com/Wariie/go-woxy/tools v0.0.0-20200723141448-9ec36fce8314 // indirect
+	github.com/gin-gonic/gin v1.6.3
+)
---
+++ main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	com "github.com/Wariie/go-woxy/com"
+	modbase "github.com/Wariie/go-woxy/modbase"
+)
+
+var mod *modbase.ModuleImpl
+
+func main() {
+	var m modbase.ModuleImpl
+	mod = &m
+	mod.Name = "hub"
+	mod.InstanceName = "hub-module"
+	modbase.HubAddress = "localhost"
+	modbase.ModulePort = "2001"
+
+	mod.Init()
+	mod.Register("GET", "/hub", index, "WEB")
+	mod.Run()
+}
+
+func index(ctx *gin.Context) {
+
+	var mods map[string]Module
+	mods = getModules(modbase.GetModManager().GetMod())
+	log.Println(mods)
+
+	running := 0
+	for k := range mods {
+		if mods[k].STATE == Online {
+			running++
+		}
+	}
+
+	ctx.HTML(http.StatusOK, "index", gin.H{
+		"title":              "mod-manager",
+		"path":               "/hub",
+		"mod_number":         len(mods),
+		"mod_number_running": running,
+		"mods":               mods,
+	})
+	log.Println("GET / mod.v0", ctx.Request.RemoteAddr)
+}
+
+func getModules(m *modbase.ModuleImpl) map[string]Module {
+	var cr com.CommandRequest
+	cr.Generate(m.Name, "hub", "Get:List:Module")
+	srv := com.Server{IP: modbase.HubAddress, Port: modbase.HubPort, Path: "", Protocol: "http"}
+	var mods map[string]Module
+	jsonData, err := com.SendRequest(srv, &cr, true)
+	if err != nil {
+		log.Println("Error retrieving Modules :", err)
+	}
+	json.NewDecoder(bytes.NewBufferString(jsonData)).Decode(&mods)
+	return mods
+}
+
+/*Module - Module configuration */
+type Module struct {
+	NAME    string
+	VERSION int
+	TYPES   string
+	EXE     ModuleExecConfig
+	BINDING ServerConfig
+	STATE   ModuleState
+	pk      string
+}
+
+/*ModuleExecConfig - Module exec file informations */
+type ModuleExecConfig struct {
+	SRC  string
+	MAIN string
+	BIN  string
+}
+
+/*ServerConfig - Server configuration*/
+type ServerConfig struct {
+	ADDRESS  string
+	PORT     string
+	PATH     []Route
+	PROTOCOL string
+	ROOT     string
+}
+
+// Route - Route redirection
+type Route struct {
+	FROM string
+	TO   string
+}
+
+//ModuleState - State of ModuleConfig
+type ModuleState string
+
+const (
+	Unknown    ModuleState = "UNKNOWN"
+	Loading    ModuleState = "LOADING"
+	Online     ModuleState = "ONLINE"
+	Stopped    ModuleState = "STOPPED"
+	Downloaded ModuleState = "DOWNLOADED"
+	Error      ModuleState = "ERROR"
+)
